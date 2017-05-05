a6770222020c2b5a7dd02d16124b17d8e67030e6
zengdaqian <zengdaqian@zengdaqiandeiMac.local>
2017-05-05 17:35:51 +0800

    add navigator view

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <nav-view></nav-view>
+    <post-list-view></post-list-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import NavView from './NavigatorView'
+  import PostListView from './PostListView'
+  export default {
+    components: {
+      PostListView,
+      NavView},
+    name: 'app'
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/PostListView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="post-list-view">
+    <h1>Title</h1>
+    <p>This is the article content.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'post-list-view'
+  }
+</script>
